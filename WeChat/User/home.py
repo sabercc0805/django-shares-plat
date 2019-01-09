3635a27d4a2e2ce1ada189995eb32cdcdb5a2dd8
sabercc <zhangzhr@daheng-image.com>
2019-01-09 16:29:17 +0800

    添加登录session验证，装饰器非登录页面跳转

--- WeChat/User/home.py
+++ WeChat/User/home.py
@@ -1,5 +1,6 @@
 import os
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,redirect
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
@@ -9,17 +10,21 @@ from .forms import UserForm
 
 @csrf_exempt
 @csrf_protect
+@login_required
 
 def homepage(request):
     return render(request, "form.html")
 
 
 def index(request):
-    pass
-    return render(request, 'base.html')
+   return render(request, 'index.html')
+
 
 
 def login(request):
+    if request.session.get('is_login',None):
+        return redirect('/index/')
+
     if request.method == "POST":
         login_form = UserForm(request.POST)
         message = "请检查填写的内容！"
@@ -29,6 +34,9 @@ def login(request):
             try:
                 user = models.Commonuser.objects.get(userid=username)
                 if user.password == password:
+                    request.session['is_login'] = True
+                    request.session['user_id'] = user.userid
+                    request.session['user_name'] = user.userid
                     return redirect('/index/')
                 else:
                     message = "密码不正确！"
@@ -38,14 +46,18 @@ def login(request):
     login_form = UserForm()
     return render(request, 'login.html', locals())
 
-
+def logout(request):
+    if not request.session.get('is_login', None):
+        # 如果本来就未登录，也就没有登出一说
+        return redirect("/index/")
+    request.session.flush()
+    # 或者使用下面的方法
+    # del request.session['is_login']
+    # del request.session['user_id']
+    # del request.session['user_name']
+    return redirect("/index/")
 
 
 def register(request):
     pass
     return render(request, 'register.html')
-
-
-def logout(request):
-    pass
-    return redirect('/index/')
